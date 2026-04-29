d8596105bfc8a49bb8c44ef4229d1e1637db0bbb
Daniel Guilherme <158202857+DK-On@users.noreply.github.com>
2026-04-28 23:01:35 -0300

    Update Principal.jsx

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { auth, db } from "../firebaseConfig";
+import { auth, db } from "../firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
