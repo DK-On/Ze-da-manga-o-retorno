2f08354b53c8d31afe15a33048813b364c4edbce
Daniel Guilherme <158202857+DK-On@users.noreply.github.com>
2026-04-28 21:36:47 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,18 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmxLIUWMLOGA5z0x0Lo43frz6ihTSOEMY",
-  authDomain: "projeto-web-daniel.firebaseapp.com",
-  projectId: "projeto-web-daniel",
-  storageBucket: "projeto-web-daniel.firebasestorage.app",
-  messagingSenderId: "697918242620",
-  appId: "1:697918242620:web:df7f6194330dfde9032839",
-  measurementId: "G-S00PTZCWJ7"
+  // Isto diz ao código para ir buscar a chave correta às definições da Vercel
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta para as páginas de Cadastro, Login e Principal usarem
+// Exporta para as páginas de Cadastro, Login e Principal utilizarem
 export const auth = getAuth(app);
 export const db = getFirestore(app);
