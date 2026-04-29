55026716edcada94e002289376ccb86327e6509d
Daniel Guilherme <158202857+DK-On@users.noreply.github.com>
2026-04-28 23:01:16 -0300

    Update import path for firebase configuration

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { auth, db } from "../firebaseConfig";
+import { auth, db } from "../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
